6efba0d6ce7c5c64293f94bc837d0a25c2484ea2
Alexey-og <54952300+Alexey-og@users.noreply.github.com>
2022-07-12 02:12:52 +0300

    Выделение логотипа в отдельный компонент. Подключение к компонентам страниц

--- project/src/pages/main-empty-screen/main-empty-screen.tsx
+++ project/src/pages/main-empty-screen/main-empty-screen.tsx
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom';
+import Logo from '../../components/logo/logo';
 
 function MainEmptyScreen(): JSX.Element {
   return (
@@ -7,9 +8,7 @@ function MainEmptyScreen(): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <Link className="header__logo-link header__logo-link--active" to="">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </Link>
+              <Logo />
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
--- project/src/pages/property-not-logged-screen/property-not-logged-screen.tsx
+++ project/src/pages/property-not-logged-screen/property-not-logged-screen.tsx
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom';
+import Logo from '../../components/logo/logo';
 
 function PropertyNotLoggedScreen(): JSX.Element {
   return (
@@ -7,9 +8,7 @@ function PropertyNotLoggedScreen(): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <Link className="header__logo-link" to="/">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </Link>
+              <Logo />
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">

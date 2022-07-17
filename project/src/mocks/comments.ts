bf6ca5cdc603ccdf2e42594909f8181ce734bf0e
Alexey-og <54952300+Alexey-og@users.noreply.github.com>
2022-07-18 00:50:59 +0300

    Создание файлов с моками отелей и отзывов

---
+++ project/src/mocks/comments.ts
@@ -0,0 +1,420 @@
+import { Comment } from '../types/comment';
+
+export const comments: Comment[] = [
+  {
+    'hotelId': 1,
+    'id': 1,
+    'user': {
+      'id': 12,
+      'isPro': true,
+      'name': 'Isaac',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/3.jpg'
+    },
+    'rating': 4,
+    'comment': 'We loved it so much, the house, the veiw, the location just great.. Highly reccomend :)',
+    'date': '2022-06-13T12:25:36.938Z'
+  },
+  {
+    'hotelId': 2,
+    'id': 1,
+    'user': {
+      'id': 18,
+      'isPro': true,
+      'name': 'Sophie',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/9.jpg'
+    },
+    'rating': 4,
+    'comment': 'Home is amazing. It\'s like staying in a museum. The rooms, furnishings and artworks are incredible. The views of My Vesuvius',
+    'date': '2022-05-25T12:25:36.939Z'
+  },
+  {
+    'hotelId': 2,
+    'id': 2,
+    'user': {
+      'id': 18,
+      'isPro': true,
+      'name': 'Sophie',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/9.jpg'
+    },
+    'rating': 5,
+    'comment': 'I stayed here for one night and it was an unpleasant experience.',
+    'date': '2022-05-25T12:25:36.939Z'
+  },
+  {
+    'hotelId': 3,
+    'id': 1,
+    'user': {
+      'id': 11,
+      'isPro': false,
+      'name': 'Jack',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/2.jpg'
+    },
+    'rating': 3,
+    'comment': 'Home is amazing. It\'s like staying in a museum. The rooms, furnishings and artworks are incredible. The views of My Vesuvius',
+    'date': '2022-06-05T12:25:36.939Z'
+  },
+  {
+    'hotelId': 3,
+    'id': 2,
+    'user': {
+      'id': 13,
+      'isPro': false,
+      'name': 'Zak',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/4.jpg'
+    },
+    'rating': 3,
+    'comment': 'Bathed in the nature. Completely unplugged. Unforgettable.',
+    'date': '2022-06-05T12:25:36.939Z'
+  },
+  {
+    'hotelId': 4,
+    'id': 1,
+    'user': {
+      'id': 17,
+      'isPro': false,
+      'name': 'Emely',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/8.jpg'
+    },
+    'rating': 4,
+    'comment': 'Bathed in the nature. Completely unplugged. Unforgettable.',
+    'date': '2022-06-13T12:25:36.939Z'
+  },
+  {
+    'hotelId': 6,
+    'id': 1,
+    'user': {
+      'id': 15,
+      'isPro': false,
+      'name': 'Kendall',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/6.jpg'
+    },
+    'rating': 2,
+    'comment': 'I stayed here for one night and it was an unpleasant experience.',
+    'date': '2022-06-01T12:25:36.939Z'
+  },
+  {
+    'hotelId': 7,
+    'id': 1,
+    'user': {
+      'id': 18,
+      'isPro': true,
+      'name': 'Sophie',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/9.jpg'
+    },
+    'rating': 5,
+    'comment': 'Home is amazing. It\'s like staying in a museum. The rooms, furnishings and artworks are incredible. The views of My Vesuvius',
+    'date': '2022-06-06T12:25:36.939Z'
+  },
+  {
+    'hotelId': 7,
+    'id': 2,
+    'user': {
+      'id': 16,
+      'isPro': true,
+      'name': 'Mollie',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/7.jpg'
+    },
+    'rating': 4,
+    'comment': 'The house is very good, very happy, hygienic and simple living conditions around it are also very good. I hope to have the opportunity to come back. Thank you.',
+    'date': '2022-06-06T12:25:36.939Z'
+  },
+  {
+    'hotelId': 7,
+    'id': 3,
+    'user': {
+      'id': 18,
+      'isPro': true,
+      'name': 'Sophie',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/9.jpg'
+    },
+    'rating': 2,
+    'comment': 'The room was spacious and clean. The pool looked nothing like the photos and desparately needs a clean. The sauna and spa were closed for lunar new year holiday.',
+    'date': '2022-06-06T12:25:36.939Z'
+  },
+  {
+    'hotelId': 8,
+    'id': 1,
+    'user': {
+      'id': 12,
+      'isPro': true,
+      'name': 'Isaac',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/3.jpg'
+    },
+    'rating': 3,
+    'comment': 'The deluxe room was a quite comfortable one with all the adequate facilities. The only thing that made me feel uncomfortable was the rude behavior of an impolite staff at the reception desk.',
+    'date': '2022-06-23T12:25:36.939Z'
+  },
+  {
+    'hotelId': 8,
+    'id': 2,
+    'user': {
+      'id': 11,
+      'isPro': false,
+      'name': 'Jack',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/2.jpg'
+    },
+    'rating': 5,
+    'comment': 'I stayed here for one night and it was an unpleasant experience.',
+    'date': '2022-06-23T12:25:36.939Z'
+  },
+  {
+    'hotelId': 9,
+    'id': 1,
+    'user': {
+      'id': 15,
+      'isPro': false,
+      'name': 'Kendall',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/6.jpg'
+    },
+    'rating': 2,
+    'comment': 'The house is very good, very happy, hygienic and simple living conditions around it are also very good. I hope to have the opportunity to come back. Thank you.',
+    'date': '2022-06-21T12:25:36.939Z'
+  },
+  {
+    'hotelId': 10,
+    'id': 1,
+    'user': {
+      'id': 11,
+      'isPro': false,
+      'name': 'Jack',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/2.jpg'
+    },
+    'rating': 4,
+    'comment': 'What an amazing view! The house is stunning and in an amazing location. The large glass wall had an amazing view of the river!',
+    'date': '2022-06-09T12:25:36.939Z'
+  },
+  {
+    'hotelId': 11,
+    'id': 1,
+    'user': {
+      'id': 17,
+      'isPro': false,
+      'name': 'Emely',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/8.jpg'
+    },
+    'rating': 3,
+    'comment': 'The house is very good, very happy, hygienic and simple living conditions around it are also very good. I hope to have the opportunity to come back. Thank you.',
+    'date': '2022-06-08T12:25:36.939Z'
+  },
+  {
+    'hotelId': 11,
+    'id': 2,
+    'user': {
+      'id': 16,
+      'isPro': true,
+      'name': 'Mollie',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/7.jpg'
+    },
+    'rating': 3,
+    'comment': 'What an amazing view! The house is stunning and in an amazing location. The large glass wall had an amazing view of the river!',
+    'date': '2022-06-08T12:25:36.939Z'
+  },
+  {
+    'hotelId': 12,
+    'id': 1,
+    'user': {
+      'id': 16,
+      'isPro': true,
+      'name': 'Mollie',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/7.jpg'
+    },
+    'rating': 4,
+    'comment': 'Bathed in the nature. Completely unplugged. Unforgettable.',
+    'date': '2022-05-26T12:25:36.939Z'
+  },
+  {
+    'hotelId': 13,
+    'id': 1,
+    'user': {
+      'id': 13,
+      'isPro': false,
+      'name': 'Zak',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/4.jpg'
+    },
+    'rating': 4,
+    'comment': 'The house is very good, very happy, hygienic and simple living conditions around it are also very good. I hope to have the opportunity to come back. Thank you.',
+    'date': '2022-06-11T12:25:36.939Z'
+  },
+  {
+    'hotelId': 14,
+    'id': 1,
+    'user': {
+      'id': 15,
+      'isPro': false,
+      'name': 'Kendall',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/6.jpg'
+    },
+    'rating': 3,
+    'comment': 'Beautiful space, fantastic location and atmosphere, really a wonderful place to spend a few days. Will be back.',
+    'date': '2022-06-09T12:25:36.939Z'
+  },
+  {
+    'hotelId': 14,
+    'id': 2,
+    'user': {
+      'id': 12,
+      'isPro': true,
+      'name': 'Isaac',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/3.jpg'
+    },
+    'rating': 3,
+    'comment': 'We loved it so much, the house, the veiw, the location just great.. Highly reccomend :)',
+    'date': '2022-06-09T12:25:36.939Z'
+  },
+  {
+    'hotelId': 15,
+    'id': 1,
+    'user': {
+      'id': 17,
+      'isPro': false,
+      'name': 'Emely',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/8.jpg'
+    },
+    'rating': 4,
+    'comment': 'Home is amazing. It\'s like staying in a museum. The rooms, furnishings and artworks are incredible. The views of My Vesuvius',
+    'date': '2022-05-27T12:25:36.939Z'
+  },
+  {
+    'hotelId': 15,
+    'id': 2,
+    'user': {
+      'id': 19,
+      'isPro': false,
+      'name': 'Christina',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/10.jpg'
+    },
+    'rating': 3,
+    'comment': 'Home is amazing. It\'s like staying in a museum. The rooms, furnishings and artworks are incredible. The views of My Vesuvius',
+    'date': '2022-05-27T12:25:36.939Z'
+  },
+  {
+    'hotelId': 15,
+    'id': 3,
+    'user': {
+      'id': 18,
+      'isPro': true,
+      'name': 'Sophie',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/9.jpg'
+    },
+    'rating': 3,
+    'comment': 'This villa is perfect in every way: the view on mountains and waterfalls, the hot tub and the villa itself. The evening here became a great continuation of our journeys over country.',
+    'date': '2022-05-27T12:25:36.939Z'
+  },
+  {
+    'hotelId': 16,
+    'id': 1,
+    'user': {
+      'id': 16,
+      'isPro': true,
+      'name': 'Mollie',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/7.jpg'
+    },
+    'rating': 5,
+    'comment': 'Home is amazing. It\'s like staying in a museum. The rooms, furnishings and artworks are incredible. The views of My Vesuvius',
+    'date': '2022-06-04T12:25:36.939Z'
+  },
+  {
+    'hotelId': 18,
+    'id': 1,
+    'user': {
+      'id': 17,
+      'isPro': false,
+      'name': 'Emely',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/8.jpg'
+    },
+    'rating': 4,
+    'comment': 'What an amazing view! The house is stunning and in an amazing location. The large glass wall had an amazing view of the river!',
+    'date': '2022-06-18T12:25:36.939Z'
+  },
+  {
+    'hotelId': 18,
+    'id': 2,
+    'user': {
+      'id': 11,
+      'isPro': false,
+      'name': 'Jack',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/2.jpg'
+    },
+    'rating': 5,
+    'comment': 'What an amazing view! The house is stunning and in an amazing location. The large glass wall had an amazing view of the river!',
+    'date': '2022-06-18T12:25:36.939Z'
+  },
+  {
+    'hotelId': 19,
+    'id': 1,
+    'user': {
+      'id': 11,
+      'isPro': false,
+      'name': 'Jack',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/2.jpg'
+    },
+    'rating': 2,
+    'comment': 'We loved it so much, the house, the veiw, the location just great.. Highly reccomend :)',
+    'date': '2022-06-22T12:25:36.939Z'
+  },
+  {
+    'hotelId': 19,
+    'id': 2,
+    'user': {
+      'id': 17,
+      'isPro': false,
+      'name': 'Emely',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/8.jpg'
+    },
+    'rating': 3,
+    'comment': 'The deluxe room was a quite comfortable one with all the adequate facilities. The only thing that made me feel uncomfortable was the rude behavior of an impolite staff at the reception desk.',
+    'date': '2022-06-22T12:25:36.939Z'
+  },
+  {
+    'hotelId': 19,
+    'id': 3,
+    'user': {
+      'id': 13,
+      'isPro': false,
+      'name': 'Zak',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/4.jpg'
+    },
+    'rating': 3,
+    'comment': 'Home is amazing. It\'s like staying in a museum. The rooms, furnishings and artworks are incredible. The views of My Vesuvius',
+    'date': '2022-06-22T12:25:36.939Z'
+  },
+  {
+    'hotelId': 20,
+    'id': 1,
+    'user': {
+      'id': 13,
+      'isPro': false,
+      'name': 'Zak',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/4.jpg'
+    },
+    'rating': 4,
+    'comment': 'The room was spacious and clean. The pool looked nothing like the photos and desparately needs a clean. The sauna and spa were closed for lunar new year holiday.',
+    'date': '2022-06-02T12:25:36.939Z'
+  },
+  {
+    'hotelId': 20,
+    'id': 2,
+    'user': {
+      'id': 13,
+      'isPro': false,
+      'name': 'Zak',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/4.jpg'
+    },
+    'rating': 5,
+    'comment': 'The room was spacious and clean. The pool looked nothing like the photos and desparately needs a clean. The sauna and spa were closed for lunar new year holiday.',
+    'date': '2022-06-02T12:25:36.939Z'
+  },
+  {
+    'hotelId': 20,
+    'id': 3,
+    'user': {
+      'id': 15,
+      'isPro': false,
+      'name': 'Kendall',
+      'avatarUrl': 'https://10.react.pages.academy/static/avatar/6.jpg'
+    },
+    'rating': 5,
+    'comment': 'I stayed here for one night and it was an unpleasant experience.',
+    'date': '2022-06-02T12:25:36.939Z'
+  }
+];
